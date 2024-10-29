a96149e028987214f6feda909bcc75b9461d8a2b
Lanmgomez <islan.gomes@supplylabs.com.br>
2024-10-28 21:18:41 -0300

    refatorando layout da tela de crud e inserindo campo de pesquisa

--- src/components/crudUsers/Crud.tsx
+++ src/components/crudUsers/Crud.tsx
@@ -1,20 +1,35 @@
 import { useState, useEffect } from "react";
-import { Pagination } from "antd";
+import { Pagination, Input, Button } from "antd";
+import { useNavigate } from "react-router-dom";
 import { usersUrl, GetUsersData } from "./hooks/CrudUsersData";
+import { AudioOutlined } from "@ant-design/icons";
 import UserCard from "./UserCard/UserCard";
 import EmptyUsers from "./EmptyUsers/EmptyUsers";
 import Container from "../Container/Container";
 
 import "./Crud.scss";
 
+const { Search } = Input;
+
 const Crud = () => {
   const [showUsers, setShowUsers] = useState([]);
   const [page, setPage] = useState(1);
 
-  const itensPerPage = 3;
+  const navigate = useNavigate();
+
+  const itensPerPage = 6;
   const startIndex = (page - 1) * itensPerPage;
   const endIndex = startIndex + itensPerPage;
   const paginatedUsersCrud = showUsers.slice(startIndex, endIndex);
+
+  const suffix = (
+    <AudioOutlined
+      style={{
+        fontSize: 16,
+        color: "#1677ff",
+      }}
+    />
+  );
 
   const fetchDataUsersCrud = async () => {
     const data = await GetUsersData(usersUrl);
@@ -38,10 +53,28 @@ const Crud = () => {
       <div className="crud-container">
         <h1>CRUD Todo List</h1>
 
+        <div className="create-new-user-and-search-btn">
+          <Search
+            style={{ width: 500 }}
+            placeholder="Pesquisar Usuários..."
+            onSearch={(value) => console.log(value)}
+            suffix={suffix}
+            enterButton
+          />
+
+          <Button
+            style={{ height: "40px" }}
+            type="primary"
+            onClick={() => navigate("/users/create-user")}
+          >
+            Adicionar Usuário
+          </Button>
+        </div>
+
         <UserCard showUsersCrud={paginatedUsersCrud} />
 
         <Pagination
-          pageSize={3}
+          pageSize={6}
           onChange={(page) => setPage(page)}
           current={page}
           total={showUsers.length}
